7842f76f535440569833b629cc8e58ca4dc93436
klang49 <nishi21480@iiitd.ac.in>
2023-02-09 15:02:21 +0530

    Minonr Updates in Schema

--- Illuminate_Schema.sql
+++ Illuminate_Schema.sql
@@ -1,4 +1,8 @@
 -- SQL Script to create Database Schema for Illuminate
+-- How to enter Multivalued attributes
+-- Adding derived attributes (age, time in company)
+-- Adding more constraints
+
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -13,13 +17,13 @@ USE Illuminate;
 --
 
 CREATE TABLE Employee (
-  Employee_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   First_Name VARCHAR(45) NOT NULL,
   Middle_Name VARCHAR(45),
   Last_Name VARCHAR(45) NOT NULL,
   Email VARCHAR(45) UNIQUE,
-  Login_Password VARCHAR(20) NOT NULL,
-  Mobile_Number BIGINT NOT NULL UNIQUE,
+  Login_Password VARCHAR(20) NOT ,
+  Mobile_Number VARCHAR(10) NOT NULL UNIQUE ,
   Date_Of_Birth DATE NOT NULL,
   Gender VARCHAR(6) CHECK (Gender = "Male" OR Gender = "Female" OR "Gender"),
   AGE INT NOT NULL,
@@ -38,8 +42,10 @@ CREATE TABLE Employee (
   Emergency_Contact_Name VARCHAR(20) NOT NULL,
   Salary FLOAT NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Employee_ID)
   -- KEY idx_actor_last_name (last_name)
+  -- Constraints
+  CONSTRAINT Check_Mobile_Number CHECK (Mobile_Number like '[7-9][0-9]{9}') 
+
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -47,7 +53,7 @@ CREATE TABLE Employee (
 --
 
 CREATE TABLE Customer (
-  Customer_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   First_Name VARCHAR(45) NOT NULL,
   Middle_Name VARCHAR(45),
   Last_Name VARCHAR(45) NOT NULL,
@@ -64,7 +70,6 @@ CREATE TABLE Customer (
   Country VARCHAR(15) NOT NULL,
   Pincode INT NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Customer_ID)
   -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -73,7 +78,7 @@ CREATE TABLE Customer (
 --
 
 CREATE TABLE Seller (
-  Seller_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   First_Name VARCHAR(45) NOT NULL,
   Middle_Name VARCHAR(45),
   Last_Name VARCHAR(45) NOT NULL,
@@ -89,7 +94,6 @@ CREATE TABLE Seller (
   Pincode INT NOT NULL,
  
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Seller_ID)
   -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -98,7 +102,7 @@ CREATE TABLE Seller (
 --
 
 CREATE TABLE Product (
-  Product_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY ,
   Product_Name VARCHAR(45) NOT NULL,
   Product_Description VARCHAR(100) NOT NULL,
   Product_Price FLOAT NOT NULL,
@@ -107,7 +111,6 @@ CREATE TABLE Product (
   Product_Ingredients VARCHAR(100) NOT NULL,
   
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Product_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -116,12 +119,11 @@ CREATE TABLE Product (
 --
 
 CREATE TABLE Product_Category (
-  Category_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Category_Name VARCHAR(45) NOT NULL,
   Category_Description VARCHAR(100) NOT NULL,
  
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Category_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -130,13 +132,12 @@ CREATE TABLE Product_Category (
 --
 
 CREATE TABLE Review (
-  Review_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Review_Rating INT NOT NULL,
   Review_Title VARCHAR(100) NOT NULL,
   Comments VARCHAR(200),
   Review_Date DATE NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Review_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -145,14 +146,13 @@ CREATE TABLE Review (
 --
 
 CREATE TABLE Brand (
-  Brand_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Brand_Name VARCHAR(50) NOT NULL,
   Brand_Description VARCHAR(200) NOT NULL,
   Brand_Logo VARCHAR(200) NOT NULL UNIQUE,
   Founder VARCHAR(50) NOT NULL,
   Country_Of_Origin VARCHAR(50) NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Brand_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --
@@ -160,12 +160,11 @@ CREATE TABLE Brand (
 --
 
 CREATE TABLE Cart (
-  Cart_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Quantity INT NOT NULL,
   Total_Amount FLOAT NOT NULL,
   Discount FLOAT, 
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Cart_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --
@@ -173,9 +172,8 @@ CREATE TABLE Cart (
 --
 
 CREATE TABLE Wishlist (
-  Wishlist_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Wishlist_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -184,14 +182,13 @@ CREATE TABLE Wishlist (
 --
 
 CREATE TABLE Customer_Orders (
-  Order_ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Order_Date DATE NOT NULL,
   Amount FLOAT NOT NULL,
   Order_Status VARCHAR(20) CHECK (Order_Status = "Delivered" OR Order_Status = "Under Process" ),
   Delivery_Date DATE NOT NULL,
   Delivery_Fee FLOAT NOT NULL,  
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Order_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -204,7 +201,6 @@ CREATE TABLE Courier (
   Courier_Name VARCHAR(50) NOT NULL,
   Tracking_URL VARCHAR(200) NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Tracking_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -213,14 +209,13 @@ CREATE TABLE Courier (
 --
 
 CREATE TABLE Customer_Transaction (
-  Transaction_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID VARCHAR(36) PRIMARY KEY,
   Transaction_Date DATE NOT NULL,
   Amount FLOAT NOT NULL,
   Transaction_Status VARCHAR(20) CHECK (Transaction_Status = "Successful" OR Transaction_Status = "Pending" OR Transaction_Status = "Failed"),
   Payment_Offers VARCHAR(50),
   Payment_Method VARCHAR(30) CHECK (Payment_Method = "Credit/Debit Card" OR Payment_Method = "Netbanking" OR Payment_Method = "UPI" OR Payment_Method = "Cash On Delivery" OR Payment_Method = "Pay Later"), 
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (Transaction_ID)
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

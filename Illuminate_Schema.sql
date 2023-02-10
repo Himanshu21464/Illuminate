7ac3ff65cadc3b45f5da7e596edfa2a29b6cddd2
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2023-02-10 22:01:09 +0530

    Add files via upload

--- Illuminate_Schema.sql
+++ Illuminate_Schema.sql
@@ -21,32 +21,34 @@ CREATE TABLE Employee (
   Login_Password VARCHAR(20) NOT NULL,
   Date_of_Birth DATE NOT NULL,
   Gender ENUM('Male', 'Female','Other') NOT NULL,
+  Age INT NOT NULL DEFAULT 0,
   House_Number INT NOT NULL,
   Locality VARCHAR(20) NOT NULL,
-  City VARCHAR(15) NOT NULL,
-  State_ VARCHAR(15) NOT NULL,
-  Country VARCHAR(15) NOT NULL,
-  Pincode INT NOT NULL,
-  Employee_Role VARCHAR(30) NOT NULL,
+  City VARCHAR(45) NOT NULL,
+  State_ VARCHAR(45) NOT NULL,
+  Country VARCHAR(45) NOT NULL,
+  Pincode VARCHAR(18) NOT NULL,
+  Employee_Role VARCHAR(50) NOT NULL,
   Date_of_Hiring DATE NOT NULL,
+  Time_in_Company INT NOT NULL DEFAULT 0,
   PAN VARCHAR(15) NOT NULL,
+  -- Blood_Group VARCHAR(20) NOT NULL,
   Blood_Group ENUM("A+", "B+", "O+", "AB+", "A-", "B-", "O-", "AB-"),
-  Emergency_Contact_Number VARCHAR(11) NOT NULL ,
-  Emergency_Contact_Name VARCHAR(20) NOT NULL,
+  Emergency_Contact_Number BIGINT NOT NULL,
+  Emergency_Contact_Name VARCHAR(255) NOT NULL,
   Salary FLOAT NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   -- KEY idx_actor_last_name (last_name)
   -- Constraints
-  CONSTRAINT Check_Emergency_Contact_Number CHECK (Emergency_Contact_Number like '^([7-9][0-9]{9},)*[7-9][0-9]{9}$'),
-  -- CONSTRAINT Check_Mobile_Number CHECK (Mobile_Number like '^([7-9][0-9]{9},)*[7-9][0-9]{9}$'),
+  CONSTRAINT Check_Emergency_Contact_Number CHECK (Emergency_Contact_Number >= 1100000000 AND Emergency_Contact_Number <= 9999999999),
   CONSTRAINT Check_Employee_Email CHECK (Email like '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\.[a-zA-Z]{2,4}$')
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Employee_Mobile_Numbers (
   Employee_ID VARCHAR(36),
-  Mobile_Number VARCHAR(20) UNIQUE,
+  Mobile_Number BIGINT UNIQUE,
   PRIMARY KEY (Employee_ID, Mobile_Number),
-  CONSTRAINT Check_Employee_Mobile_Number CHECK (Mobile_Number like '^[7-9][0-9]{9}$'),
+  CONSTRAINT Check_Employee_Mobile_Number CHECK (Mobile_Number >= 1100000000 AND Mobile_Number <= 9999999999),
   FOREIGN KEY (Employee_ID) REFERENCES Employee (ID) ON DELETE CASCADE
   
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -82,23 +84,22 @@ CREATE TABLE Customer (
   Gender ENUM('Male', 'Female','Other') NOT NULL,
   Age INT NOT NULL DEFAULT 0,
   House_Number INT NOT NULL,
-  Locality VARCHAR(20) NOT NULL,
-  City VARCHAR(15) NOT NULL,
-  State_ VARCHAR(15) NOT NULL,
-  Country VARCHAR(15) NOT NULL,
-  Pincode INT NOT NULL,
-  Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  -- CONSTRAINT Check_Customer_Mobile_Number CHECK (Mobile_Number like '^([7-9][0-9]{9},)*[7-9][0-9]{9}$'),
-  CONSTRAINT Check_Customer_Email CHECK (Email like '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\.[a-zA-Z]{2,4}$')
+  Locality VARCHAR(40) NOT NULL,
+  City VARCHAR(45) NOT NULL,
+  State_ VARCHAR(45) NOT NULL,
+  Country VARCHAR(45) NOT NULL,
+  Pincode VARCHAR(18) NOT NULL,
+  Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  -- CONSTRAINT Check_Customer_Email CHECK (Email like '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\.[a-zA-Z]{2,4}$')
   -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Customer_Mobile_Numbers (
   Customer_ID VARCHAR(36),
-  Mobile_Number VARCHAR(20) UNIQUE,
+  Mobile_Number BIGINT UNIQUE,
   PRIMARY KEY (Customer_ID, Mobile_Number),
   FOREIGN KEY (Customer_ID) REFERENCES Customer (ID) ON DELETE CASCADE,
-  CONSTRAINT Check_Customer_Mobile_Number CHECK (Mobile_Number like '^[7-9][0-9]{9}$')
+  CONSTRAINT Check_Customer_Mobile_Number CHECK (Mobile_Number >= 1100000000 AND Mobile_Number <= 9999999999)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -125,29 +126,29 @@ CREATE TABLE Seller (
   Last_Name VARCHAR(45) NOT NULL,
   Email VARCHAR(45) UNIQUE,
   Login_Password VARCHAR(20) NOT NULL,
-  Mobile_Number VARCHAR(20) NOT NULL ,
+  Mobile_Number BIGINT NOT NULL ,
   Company_Name VARCHAR(40) NOT NULL,
   Property_Number INT NOT NULL,
   Locality VARCHAR(20) NOT NULL,
-  City VARCHAR(15) NOT NULL,
-  State_ VARCHAR(15) NOT NULL,
-  Country VARCHAR(15) NOT NULL,
-  Pincode INT NOT NULL,
+  City VARCHAR(45) NOT NULL,
+  State_ VARCHAR(45) NOT NULL,
+  Country VARCHAR(45) NOT NULL,
+  Pincode VARCHAR(18) NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  -- CONSTRAINT Check_Mobile_Number CHECK (Mobile_Number like '^([7-9][0-9]{9},)*[7-9][0-9]{9}$'),
-  CONSTRAINT Check_Seller_Email CHECK (Email like '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\.[a-zA-Z]{2,4}$')
+  CONSTRAINT Check_Mobile_Number CHECK (Mobile_Number >= 1100000000 AND Mobile_Number <=9999999999)
+  -- CONSTRAINT Check_Seller_Email CHECK (Email like '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\.[a-zA-Z]{2,4}$')
   -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE Seller_Mobile_Numbers (
-  Seller_ID VARCHAR(36),
-  Mobile_Number VARCHAR(20) UNIQUE,
-  PRIMARY KEY (Seller_ID, Mobile_Number),
-  CONSTRAINT Check_Seller_Mobile_Number CHECK (Mobile_Number like '^[7-9][0-9]{9}$'),
-  FOREIGN KEY (Seller_ID) REFERENCES Seller (ID) ON DELETE CASCADE
-  
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- CREATE TABLE Seller_Mobile_Numbers (
+--  Seller_ID VARCHAR(36),
+--  Mobile_Number VARCHAR(20) UNIQUE,
+--  PRIMARY KEY (Seller_ID, Mobile_Number),
+--  CONSTRAINT Check_Seller_Mobile_Number CHECK (Mobile_Number like '^[7-9][0-9]{9}$'),
+--  FOREIGN KEY (Seller_ID) REFERENCES Seller (ID) ON DELETE CASCADE
+--  
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --
 -- View for Seller_Details
 --
@@ -163,12 +164,12 @@ FROM Seller s;
 
 CREATE TABLE Product (
   ID VARCHAR(36) PRIMARY KEY ,
-  Product_Name VARCHAR(45) NOT NULL,
-  Product_Description VARCHAR(100) NOT NULL,
+  Product_Name VARCHAR(100) NOT NULL,
+  Product_Description VARCHAR(255) NOT NULL,
   Product_Price FLOAT NOT NULL,
   Product_Quantity INT NOT NULL,
   Product_Images VARCHAR(150) NOT NULL,
-  Product_Ingredients VARCHAR(100) NOT NULL,
+  Product_Ingredients VARCHAR(200) NOT NULL,
   CATEGORY_ID VARCHAR(36),
   BRAND_ID VARCHAR(36),
   SELLER_ID VARCHAR(36),
@@ -187,8 +188,7 @@ CREATE TABLE Product (
 CREATE TABLE Category(
   ID VARCHAR(36) PRIMARY KEY,
   CategoryName VARCHAR(45) NOT NULL,
-  CategoryDescription VARCHAR(100) NOT NULL,
- 
+  CategoryDescription VARCHAR(255) NOT NULL,
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
  -- KEY idx_actor_last_name (last_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -201,7 +201,7 @@ CREATE TABLE Review (
   ID VARCHAR(36) PRIMARY KEY,
   Review_Rating INT NOT NULL,
   Review_Title VARCHAR(100) NOT NULL,
-  Comments VARCHAR(200),
+  Comments VARCHAR(255),
   Review_Date DATE NOT NULL,
   Product_ID VARCHAR(36),
   Customer_ID VARCHAR(36),
@@ -231,7 +231,7 @@ DELIMITER ;
 CREATE TABLE Brand (
   ID VARCHAR(36) PRIMARY KEY,
   Brand_Name VARCHAR(50) NOT NULL,
-  Brand_Description VARCHAR(200) NOT NULL,
+  Brand_Description VARCHAR(255) NOT NULL,
   Brand_Logo VARCHAR(200) NOT NULL UNIQUE,
   Founder VARCHAR(50) NOT NULL,
   Country_Of_Origin VARCHAR(50) NOT NULL,
@@ -290,9 +290,9 @@ CREATE TABLE Orders (
 --
 
 CREATE TABLE Courier (
-  Tracking_ID INT UNSIGNED NOT NULL UNIQUE,
+  Tracking_ID VARCHAR(36) PRIMARY KEY,
   Courier_Name VARCHAR(50) NOT NULL,
-  Tracking_URL VARCHAR(200) NOT NULL,
+  Tracking_URL VARCHAR(255) NOT NULL,
   Order_ID VARCHAR(36),
   FOREIGN KEY (Order_ID) REFERENCES Orders(ID),
   Last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -343,9 +343,3 @@ CREATE INDEX idx_Customer_Order_Date ON Orders (Order_Date);
 CREATE INDEX idx_Order_Delivery_Date ON Orders (Delivery_Date);
 
 CREATE INDEX idx_Order_Review_Rating ON Review (Review_Rating);
-
-
-
-
-
-
